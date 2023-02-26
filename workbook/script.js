377d9b672b62cdcc653fbdf34be9f4b41b70bfc7
Nickita <turrtopsis@gmail.com>
2023-02-26 19:24:42 +0300

    add workbook

---
+++ workbook/script.js
@@ -0,0 +1,45 @@
+//Условия
+
+// let num = 50;
+
+// if (num > 40) {
+//     console.log('OK');
+// } else 
+//     console.log('Error');
+
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('В Точку');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//     break;
+// };
+
+//Циклы
+
+// let num = 50;
+//     while (num <= 50) {
+//         console.log(num);
+//         num++;
+//     };
+
+//     do {
+//         console.log(num);
+//         num++;
+//     } while (num < 55);
+
+
+//     for (let i = 1; i < 8; i++) {
+//         console.log(i);
+//     };
+
+// break; - прерывание цикла
+// continue; - пропуск шага цикла
