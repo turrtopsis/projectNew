ab6127150bf78cf8204515d142bc3028797c10b7
Nickita <turrtopsis@gmail.com>
2023-02-26 20:00:18 +0300

    workbook

--- workbook/script.js
+++ workbook/script.js
@@ -43,3 +43,29 @@
 
 // break; - прерывание цикла
 // continue; - пропуск шага цикла
+
+//Вложенные циклы
+
+// let result = '';
+// const lenght = 7;
+
+// for (let i = 0; i < lenght; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// };
+// console.log(result);
+
+//Метки
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level ${j}`);
+//         for (let k = 0; k < 5; k++) {
+//             if (k === 2) continue first;
+//             console.log(`Third level ${k}`);
+//         }
+//     }
+// }
