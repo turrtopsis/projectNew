9f192232b997669565033382f9aa6454b6c438cb
Nickita <turrtopsis@gmail.com>
2023-02-28 15:45:25 +0300

    add workbook 'Object'

--- workbook/script.js
+++ workbook/script.js
@@ -118,3 +118,20 @@
 // promotion(convert(500, usdCurr));
 
 
+// Перебор
+// for ( let key in options) {
+
+// };
+
+// object 
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     }
+// };
+
+// console.log(Object.keys(options).length);
